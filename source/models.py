523726067bf941a0de3ea10e498b65526bbf6383
Tulley Mwenda <Tulleyteyez@gmail.com>
2024-08-05 15:26:59 +0300

    add password and signup route

--- source/models.py
+++ source/models.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 import uuid
 
+from werkzeug.security import generate_password_hash, check_password_hash
 db = SQLAlchemy()
 
 class User(db.Model):
@@ -19,6 +20,12 @@ class User(db.Model):
     referred_by = db.Column(db.String(36), db.ForeignKey('users.referral_code'), nullable=True)
 
     role = db.relationship('Role', backref=db.backref('users', lazy=True))
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 class Role(db.Model):
     __tablename__ = 'roles'
